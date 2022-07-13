e73e1868f488bd7c718149dbf66f920fe6ba57a0
fdmshd <kirh09764@gmail.com>
2022-07-13 12:56:19 +0700

    add heapsort

---
+++ Sorts/HeapSort.go
@@ -0,0 +1,59 @@
+package sorts
+
+type Heap struct {
+	a        []int
+	heapSize int
+}
+
+func (h *Heap) Parent(i int) int {
+	i = i + 1
+	parent := i/2 - 1
+	return parent
+}
+
+func (h *Heap) Left(i int) int {
+	left := 2*i + 1
+	return left
+}
+
+func (h *Heap) Right(i int) int {
+	right := 2*i + 2
+	return right
+}
+
+func (h *Heap) MaxHeapify(i int) {
+	l := h.Left(i)
+	r := h.Right(i)
+	var largest int
+	if l < h.heapSize && h.a[l] > h.a[i] {
+		largest = l
+	} else {
+		largest = i
+	}
+	if r < h.heapSize && h.a[r] > h.a[largest] {
+		largest = r
+	}
+	if largest != i {
+		h.a[i], h.a[largest] = h.a[largest], h.a[i]
+		h.MaxHeapify(largest)
+	}
+}
+
+func (h *Heap) BuildMaxHeap() {
+	h.heapSize = len(h.a)
+	for i := len(h.a)/2 - 1; i >= 0; i-- {
+		h.MaxHeapify(i)
+	}
+}
+
+func Heapsort(arr []int) {
+	h := &Heap{
+		a: arr,
+	}
+	h.BuildMaxHeap()
+	for i := len(arr) - 1; i > 0; i-- {
+		h.a[0], h.a[i] = h.a[i], h.a[0]
+		h.heapSize -= 1
+		h.MaxHeapify(0)
+	}
+}
--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"algs/mymath"
+	sorts "algs/Sorts"
 	"fmt"
 )
 
 func main() {
-	arr := []int{5, 6, 7, 1}
-	fmt.Println(mymath.Minmax(arr))
+	arr := []int{5, 6, 7, 1, 9, 8, 2, 3, 4}
+	sorts.Heapsort(arr)
+	fmt.Println(arr)
 }
