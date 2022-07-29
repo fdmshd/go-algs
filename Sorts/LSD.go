eb34087990d121013f647fbfc39c4b7121b57c6a
fdmshd <kirh09764@gmail.com>
2022-07-29 11:22:52 +0700

    add LSD sort

---
+++ Sorts/LSD.go
@@ -0,0 +1,42 @@
+package sorts
+
+func LCD(arr []int) {
+	n := len(arr)
+	m := countDigs(arr[0])
+	newArr := make([]int, n)
+	for j := m; j >= 0; j-- {
+		p := make([]int8, 10)
+		for i := 0; i < n; i++ {
+			p[digit(arr[i], j, m)]++
+		}
+		for k := 1; k < len(p); k++ {
+			p[k] += p[k-1]
+		}
+		for k := len(p) - 1; k > 0; k-- {
+			p[k] = p[k-1]
+		}
+		p[0] = 0
+		for i := 0; i < n; i++ {
+			d := digit(arr[i], j, m)
+			newArr[p[d]] = arr[i]
+			p[d]++
+		}
+		copy(arr, newArr)
+	}
+}
+
+func digit(numb, dig, n int) int {
+	for i := n; i > dig; i-- {
+		numb /= 10
+	}
+	return numb % 10
+}
+
+func countDigs(num int) int {
+	digs := 0
+	for num != 0 {
+		digs++
+		num /= 10
+	}
+	return digs - 1
+}
--- main.go
+++ main.go
@@ -1,21 +1,12 @@
 package main
 
 import (
-	bst "algs/BinTrees"
+	sorts "algs/Sorts"
 	"fmt"
-	"math/rand"
-	"time"
 )
 
 func main() {
-	root := &bst.AVLNode{Val: 5}
-	var input int
-	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < 10; i++ {
-		input = rand.Intn(20)
-		root = root.Insert(input)
-	}
-
-	fmt.Println(root)
-
+	arr := []int{329, 457, 657, 839, 436, 720, 355}
+	sorts.LCD(arr)
+	fmt.Println(arr)
 }
